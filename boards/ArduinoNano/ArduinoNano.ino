d8102f237a012ca193ee39a3bb7530d3c342c235
Dion Olsthoorn <dion.olsthoorn@gmail.com>
2021-05-29 01:00:10 +0200

    wip

--- boards/ArduinoNano/ArduinoNano.ino
+++ boards/ArduinoNano/ArduinoNano.ino
@@ -1,47 +1,38 @@
 /*
  ROM dumper written by Sven Oliver Moll
- Changes for Arduino Nano version by Dion Olsthoorn
+ Adapted for Arduino Nano by Dion Olsthoorn
  ==============================================================================
 
  Distributed under the terms of the GPLv3.
-
- Sorry for using Arduino IDE, but I didn't know how else to get USB serial.
 
  Tools -> Board -> Arduino Nano
 */
 
 #define VERSION "0"
-#define DEBUG_OUTPUT 0
 #define AD_THRESHOLD 512
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(38400);
   
   /* address bus: out */
-  DDRD = DDRD | B11111100; // preserve bits 0 and 1 (used for serial comm.)
+  // The cartridge address signals A0-A11 are mapped across port registers D and B.
+  // A12 is hard wired to VCC, as there are no more free GPIOs on the Nano
+  DDRD = DDRD | B11111100; // preserve bits 0 and 1 (TX/RX)
   DDRB = B00111111; // note: bits 6 and 7 are not usable
   // see https://www.arduino.cc/en/Reference/PortManipulation
-
+  
   /* data bus: in */
   DDRC = B00000000;
 }
 
 static inline void setaddr( uint16_t addr )
 { 
-  // Note that he cartidge's A12 signal is wired to VCC, as we don't 
-  // have any more free pins on the Arduino Nano. This means that 
-  // only bankswitching schemes are supported which use hotspots within 
-  // the cartidge ROM address space (i.e. f8, f6, f4, parker, CBS, M-Network)
+  // Because the cartidge A12 signal is wired to VCC, only backswitching schemes
+  // are supported which use hotspots within the cartridge ROM address space. 
+  // So f8, f6, f4, parker, CBS, M-Network are supported, but e.g. not TigerVision's 3f.
   PORTB = addr >> 6; 
-  PORTD = (PORTD & B00000011) | (addr << 2); // preserve TX/RX bits
+  PORTD = (PORTD & B00000011) | (addr << 2); // also preserving TX/RX bits
   delay(1);
-}
-
-void printHex(int num)
-{
-  char tmp[3];
-  sprintf(tmp, "%02X", num);
-  Serial.print(tmp);
 }
 
 static inline uint8_t peek( uint16_t addr )
@@ -49,8 +40,8 @@ static inline uint8_t peek( uint16_t addr )
   setaddr( addr );
 
   uint8_t data = PINC;
-  if (analogRead(A6) > AD_THRESHOLD) data = data | B01000000;
-  if (analogRead(A7) > AD_THRESHOLD) data = data | B10000000;
+  if (analogRead(A6) > AD_THRESHOLD) data |= B01000000;
+  if (analogRead(A7) > AD_THRESHOLD) data |= B10000000;
   
   return data;
 }
@@ -72,48 +63,38 @@ uint16_t inputHex( uint16_t value, int b )
   return value;
 }
 
+void printHex(int num)
+{
+  char tmp[4];
+  sprintf(tmp, "%02X", num);
+  Serial.print(tmp);
+}
+
 void executeCmd( uint8_t cmd, uint16_t arg1, uint16_t arg2 )
 {
   uint16_t i;
-#if DEBUG_OUTPUT
-  //Serial.printf( "%c %04X %04X\n", cmd, arg1, arg2 );
-#endif
   switch( cmd )
   {
     case 'V':
       Serial.println( VERSION );
       break;
     case 'R':
-#if DEBUG_OUTPUT
-      //Serial.printf( "%04X %02X\n", arg1, peek( arg1 ) );
-#else      
-      //Serial.printf( "%02X", peek( arg1 ) );
       printHex( peek( arg1 ) );
-#endif
       break;
     case 'W':
-#if DEBUG_OUTPUT
-      Serial.println( "poke/write not supported for Arduino Nano dumper" );
-#endif
+      /* poke/write not supported for Arduino Nano dumper, so just ignore */
       break;
     case 'D':
       for( i = 0; i < arg2; ++i )
       {
-#if DEBUG_OUTPUT
-        //Serial.printf( "%04X %02X\n", arg1+i, peek( arg1+i ) );
-#else      
-        //Serial.printf( "%02X", peek( arg1+i ) );
         printHex( peek( arg1+i ) );
-#endif
       }
-#if DEBUG_OUTPUT
-      Serial.println();
-#endif
       break;
   }
 }
 
-void loop() {
+void loop() 
+{  
   int incomingByte;
   uint8_t cmd = 0;
   uint8_t pos = 0;
@@ -123,7 +104,7 @@ void loop() {
 
   while( !cmdDone )
   {
-    if (Serial.available() > 0)
+    if ( Serial.available() )
     {
       incomingByte = Serial.read();
       if( incomingByte == '\r' )
@@ -143,25 +124,16 @@ void loop() {
             case 'r':
             case 'R':
               cmd = 'R';
-#if DEBUG_OUTPUT
-              Serial.println("READ");
-#endif
               pos++;
               break;
             case 'w':
             case 'W':
               cmd = 'W';
-#if DEBUG_OUTPUT
-              Serial.println("WRITE");
-#endif
               pos++;
               break;
             case 'd':
             case 'D':
               cmd = 'D';
-#if DEBUG_OUTPUT
-              Serial.println("DUMP");
-#endif
               pos++;
               break;
             default:
@@ -176,9 +148,6 @@ void loop() {
           else
           {
             arg1 = inputHex( arg1, incomingByte );
-#if DEBUG_OUTPUT
-            Serial.println( arg1, 16 );
-#endif
           }
           break;
         case 2:
@@ -189,9 +158,6 @@ void loop() {
           else
           {
             arg2 = inputHex( arg2, incomingByte );
-#if DEBUG_OUTPUT
-            Serial.println( arg2, 16 );
-#endif
           }
           break;
         case 255:
